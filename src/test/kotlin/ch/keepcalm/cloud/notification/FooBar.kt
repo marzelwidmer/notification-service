ceb3310b26548c8f590cbfa40e21b47b7175e406
Marcel Widmer <marzelwidmer@gmail.com>
2018-10-07 10:42:46 +0200

    WIP on coroutines

--- src/test/kotlin/ch/keepcalm/cloud/notification/FooBar.kt
+++ src/test/kotlin/ch/keepcalm/cloud/notification/FooBar.kt
@@ -1,30 +1,26 @@
 package ch.keepcalm.cloud.notification
 
 import kotlinx.coroutines.experimental.GlobalScope
-import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
-
 import java.net.URL
 
+
+
 fun main(args: Array<String>) {
-    println("Hello Dude..")
-
-
-
     GlobalScope.launch {
-        delay(1000)
         println("Hello from Kotlin Coroutines!")
-    }
 
-    measureTime {
-        val jokeIdList = listOf(1, 2, 3, 4, 5, 6, 7, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-        for (jokeId in jokeIdList) {
-            getJoke(jokeId.toString())
+        measureTime {
+            for (jokeId in 1..604) {
+                println(getJoke(jokeId.toString()))
+                //val joke = Gson().fromJson(result, IcndbJoke::class.java)
+                //println(joke.value?.joke)
+            }
         }
     }
 
     // Let the main function running
-    Thread.sleep(5000)
+    Thread.sleep(70000)
 }
 
 
@@ -32,7 +28,7 @@ fun measureTime(block: () -> Unit) {
     val start = System.nanoTime()
     block()
     val end = System.nanoTime()
-    println("${(end - start) / 1.0e9} seconds")
+    println("***************  ${(end - start) / 1.0e9} seconds ***************  ")
 }
 
 fun getIp() {
@@ -40,9 +36,10 @@ fun getIp() {
     println(result)
 }
 
-fun getJoke(jokeId: String = "random") {
-    val result = URL("http://api.icndb.com/jokes/$jokeId").readText()
-    println(result)
-}
+
+fun getJoke(jokeId: String = "random") =
+        URL("http://api.icndb.com/jokes/$jokeId").readText()
+
+
 
 
