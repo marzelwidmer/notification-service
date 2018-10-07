4725b253e5b68a0b8f813f9490851168d246cc57
Marcel Widmer <marzelwidmer@gmail.com>
2018-10-07 11:18:28 +0200

    WIP on coroutines asych

--- src/test/kotlin/ch/keepcalm/cloud/notification/FooBar.kt
+++ src/test/kotlin/ch/keepcalm/cloud/notification/FooBar.kt
@@ -1,20 +1,36 @@
 package ch.keepcalm.cloud.notification
 
+import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 import java.net.URL
 
 
-
 fun main(args: Array<String>) {
+
+
     GlobalScope.launch {
         println("Hello from Kotlin Coroutines!")
 
         measureTime {
-            for (jokeId in 1..604) {
-                println(getJoke(jokeId.toString()))
-                //val joke = Gson().fromJson(result, IcndbJoke::class.java)
-                //println(joke.value?.joke)
+            val jokeListDeferred = mutableListOf<Deferred<String>>()
+            for (jokeId in 1..200) {
+                jokeListDeferred += async { "Joke for $jokeId is ${getJoke(jokeId.toString())}" }
+            }
+            for (joke in jokeListDeferred) {
+                println(joke.await())
+            }
+        }
+
+
+        measureTime {
+            val jokeList  = mutableListOf<String>()
+            for (jokeId in 1..200) {
+                jokeList += "Joke for $jokeId is ${getJoke(jokeId.toString())}"
+            }
+            for (joke in jokeList) {
+                println(joke)
             }
         }
     }
@@ -24,12 +40,17 @@ fun main(args: Array<String>) {
 }
 
 
-fun measureTime(block: () -> Unit) {
+suspend fun measureTime(block: suspend () -> Unit) {
     val start = System.nanoTime()
     block()
     val end = System.nanoTime()
     println("***************  ${(end - start) / 1.0e9} seconds ***************  ")
 }
+
+
+
+
+
 
 fun getIp() {
     val result = URL("http://api.ipify.org").readText()
